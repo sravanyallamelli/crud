7d13f3c798a16f5d1770c54014d42b7c7b1c9dcb
sravanyallamelli <sravansatyamy@gmail.com>
2022-11-24 17:51:28 +0530

    updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './layout/layout.module';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    LayoutModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
